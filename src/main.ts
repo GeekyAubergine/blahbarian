343204af2e92bd4bcc1c551ba934741d608771fb
Zoe Aubert <hello@zoeaubert.me>
2023-03-25 11:17:46 +0000

    Renderer follow player

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { renderWorld } from "./renderer";
 import "./style.css";
-import { Direction, World } from "./types";
+import { Direction, EnemyType, World } from "./types";
 
 // ur not null shut up
 const canvas: HTMLCanvasElement = document.querySelector("#game-canvas")!;
@@ -8,7 +8,16 @@ const ctx = canvas.getContext("2d")!;
 
 const world: World = {
   tiles: [],
-  enemies: [],
+  enemies: [
+    {
+      id: "2",
+      type: EnemyType.CHAIR,
+      position: { x: 0, y: -5 },
+      velocity: { x: 0, y: 0 },
+      health: 100,
+      direction: Direction.DOWN,
+    },
+  ],
   player: {
     id: "player",
     position: { x: 0, y: 0 },
@@ -22,6 +31,12 @@ function render() {
   renderWorld(canvas, ctx, world);
 
   window.requestAnimationFrame(render);
+
+//   world.player.position.x += 0.01;
+//   world.player.position.y += 0.005;
+
+//   world.enemies[0].position.x += 0.05;
+//   world.enemies[0].position.y += 0.006;
 }
 
 window.requestAnimationFrame(render);
