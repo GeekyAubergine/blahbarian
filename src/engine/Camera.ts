291142a2cb14fff2fa7c1898327b3cde83b33053
Zoe Aubert <hello@zoeaubert.me>
2023-03-29 12:51:29 +0100

    cleanup

--- src/engine/Camera.ts
+++ src/engine/Camera.ts
@@ -23,43 +23,6 @@ export class Camera {
     const target =
       this.goalPosition ?? game.getWorld().getPlayer().getPosition();
 
-    // this.goalPosition = game.getWorld().getPlayer().getPosition();
-
-    // if (Number.isNaN(game.getWorld().getPlayer().getPosition().x)) {
-    //   throw new Error("Player position x is NaN");
-    // }
-
-    // if (Number.isNaN(game.getWorld().getPlayer().getPosition().y)) {
-    //   throw new Error("Player position y is NaN");
-    // }
-
-    // const delta = this.goalPosition.sub(this.position);
-
-    // let move = delta.mul(dt);
-
-    // console.log({ move })
-
-    // let move = delta.mul(LAG_FACTOR);
-
-    // if (delta.mag() > CAMERA_MAX_SPEED_LAG) {
-    //   move = delta.normalize().mul(CAMERA_MAX_SPEED_LAG);
-    // }
-    
-    // console.log(Vector.Lerp(new Vector(12, 0), new Vector(10, 0), 0.25).toString())
-
-    // console.log('target', target.toString())
-
-    // console.log(
-    //   "target",
-    //   target.toString(),
-    //   "lerp",
-    //   Vector.Lerp(this.position, target, 0.1).toString()
-    // );
-
-    this.position = target;// this.position.add(Vector.Lerp(this.position, target, 1));
-
-    // this.scale = this.goalScale;
-
-    // console.log({ x: this.position.x, y: this.position.y });
+    this.position = target;
   }
 }
